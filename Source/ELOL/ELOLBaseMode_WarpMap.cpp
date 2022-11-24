f717155d168d324e0b3ca681217af2b9bbbd90d6
oloflomo <strzalkowski.antoni@gmail.com>
2022-11-24 03:16:49 +0100

    clean version#1

---
+++ Source/ELOL/ELOLBaseMode_WarpMap.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ELOLBaseMode_WarpMap.h"
+#include "Bop.h"
+
+AELOLBaseMode_WarpMap::AELOLBaseMode_WarpMap()
+{
+	DefaultPawnClass = ABop::StaticClass();
+}
