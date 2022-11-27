b0e27ddf06f94a0092f85fb5c9808c0962d43f66
oloflomo <strzalkowski.antoni@gmail.com>
2022-11-27 19:27:20 +0100

    laser explosions #1

--- Source/ELOL/LASER.cpp
+++ Source/ELOL/LASER.cpp
@@ -21,13 +21,38 @@ ALASER::ALASER()
 	Mesh->SetEnableGravity(0);
 
 	RootComponent = Mesh;
+
+	Capsule = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Cap"));
+	Capsule->SetRelativeLocation(FVector(0, 0, 0));
+	Capsule->SetWorldRotation(FRotator(-90, 0, 0));
+	Capsule->SetupAttachment(RootComponent);
+	Capsule->InitCapsuleSize(30.5, 30.f);
+	Capsule->OnComponentBeginOverlap.AddDynamic(this, &ALASER::OnOverlapBegin);
 }
 
 // Called when the game starts or when spawned
 void ALASER::BeginPlay()
 {
 	Super::BeginPlay();
-	lifespan = 100;
+	lifespan = 1000;
+}
+
+void ALASER::Collision()
+{
+	GetWorld()->SpawnActor<AActor>(BoomType, Mesh->GetComponentLocation(), Mesh->GetComponentRotation());
+	GetWorld()->DestroyActor(this);
+}
+
+void ALASER::OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor && (OtherActor != this) && OtherComp)
+	{
+		if (GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("overlap begin"));
+		}
+		Collision();
+	}
 }
 
 // Called every frame
--- Source/ELOL/LASER.h
+++ Source/ELOL/LASER.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "Components/StaticMeshComponent.h"
+#include "Components/CapsuleComponent.h"
 #include "LASER.generated.h"
 
 UCLASS()
@@ -16,6 +17,11 @@ public:
 	// Sets default values for this actor's properties
 	ALASER();
 
+	void Collision();
+
+	UFUNCTION()
+		void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
 protected:
 	// Called when the game starts or when spawneds
 	virtual void BeginPlay() override;
@@ -24,7 +30,13 @@ protected:
 		UStaticMeshComponent* Mesh;
 
 	UPROPERTY(EditAnywhere)
+		UCapsuleComponent* Capsule;
+
+	UPROPERTY(EditAnywhere)
 		int lifespan;
+
+	UPROPERTY(EditAnywhere)
+		TSubclassOf<AActor> BoomType;
 
 public:	
 	// Called every frame
