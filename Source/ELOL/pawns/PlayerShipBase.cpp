50da111570cdc59de6c0c85ecf7f4d0269e7f1e0
oloflomo <strzalkowski.antoni@gmail.com>
2023-03-02 00:08:57 +0100

    player ship fix #3

--- Source/ELOL/pawns/PlayerShipBase.cpp
+++ Source/ELOL/pawns/PlayerShipBase.cpp
@@ -3,3 +3,198 @@
 
 #include "PlayerShipBase.h"
 
+APlayerShipBase::APlayerShipBase()
+{
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	//Create components
+	static ConstructorHelpers::FObjectFinder<UStaticMesh>MeshAsset1(TEXT("/Script/Engine.StaticMesh'/Game/grafika/spaceship/spac3/sp2-1_Object134.sp2-1_Object134'"));
+	UStaticMesh* Asset1 = MeshAsset1.Object;
+	static ConstructorHelpers::FObjectFinder<UStaticMesh>MeshAsset2(TEXT("/Script/Engine.StaticMesh'/Game/grafika/spaceship/spac3/sp2-1_Object134_001.sp2-1_Object134_001'"));
+	UStaticMesh* Asset2 = MeshAsset2.Object;
+	static ConstructorHelpers::FObjectFinder<UStaticMesh>MeshAsset3(TEXT("/Script/Engine.StaticMesh'/Game/grafika/spaceship/spac3/sp2-1_Object134_002.sp2-1_Object134_002'"));
+	UStaticMesh* Asset3 = MeshAsset3.Object;
+
+	Root->SetStaticMesh(Asset1);
+	Root->SetSimulatePhysics(true);
+	Root->SetEnableGravity(0);
+
+	Mesh2 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh2"));
+	Mesh2->SetupAttachment(Root);
+	Mesh2->SetStaticMesh(Asset2);
+	Mesh2->SetRelativeLocation(FVector(0, 0, 0));
+	Mesh2->SetWorldRotation(FRotator(0, 0, 0));
+	Mesh2->SetSimulatePhysics(false);
+
+	Mesh3 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh3"));
+	Mesh3->SetupAttachment(Root);
+	Mesh3->SetStaticMesh(Asset3);
+	Mesh3->SetRelativeLocation(FVector(0, 0, 0));
+	Mesh3->SetWorldRotation(FRotator(0, 0, 0));
+	Mesh3->SetSimulatePhysics(false);
+
+	RootComponent = Root;
+}
+
+// Player actions
+
+void APlayerShipBase::thrusting(float timedelta)
+{
+	FVector XUnit = Root->GetRelativeRotation().Vector();
+	Root->AddForce(100000 * timedelta * XUnit);
+	//Controller.thrusting(1);
+}
+
+void APlayerShipBase::yawing(float timedelta)
+{
+	FVector YUnit = Root->GetRightVector();
+	Root->AddForceAtLocationLocal(timedelta * FVector(0, 1, 0), FVector(1000000, 0, 0));
+}
+
+void APlayerShipBase::pitching(float timedelta)
+{
+	FVector ZUnit = Root->GetUpVector();
+	Root->AddForceAtLocationLocal(timedelta * FVector(0, 0, 1), FVector(1000000, 0, 0));
+}
+
+void APlayerShipBase::rolling(float timedelta)
+{
+	FVector ZUnit = Root->GetUpVector();
+	Root->AddForceAtLocationLocal(timedelta * FVector(0, 1, 0), FVector(0, 0, 1000000));
+}
+
+void APlayerShipBase::InitFire()
+{
+	GetWorld()->SpawnActor<AActor>(BulletType, GetActorLocation() + FVector(200, -50, 0), GetActorRotation());
+	GetWorld()->SpawnActor<AActor>(BulletType, GetActorLocation() + FVector(200, 50, 0), GetActorRotation());
+}
+
+void APlayerShipBase::ShowInv()
+{
+	InvWidget->AddToViewport();
+}
+
+void APlayerShipBase::HideInv()
+{
+	InvWidget->RemoveFromViewport();
+}
+
+//server
+
+
+void APlayerShipBase::Serverthrusting_Implementation(float timedelta)
+{
+	thrusting(timedelta);
+}
+bool APlayerShipBase::Serverthrusting_Validate(float timedelta)
+{
+	return true;
+}
+
+void APlayerShipBase::ServerInitFire_Implementation()
+{
+	InitFire();
+}
+bool APlayerShipBase::ServerInitFire_Validate()
+{
+	return true;
+}
+
+void APlayerShipBase::Serverpitching_Implementation(float timedelta)
+{
+	pitching(timedelta);
+}
+bool APlayerShipBase::Serverpitching_Validate(float timedelta)
+{
+	return true;
+}
+
+void APlayerShipBase::Serveryawing_Implementation(float timedelta)
+{
+	yawing(timedelta);
+}
+bool APlayerShipBase::Serveryawing_Validate(float timedelta)
+{
+	return true;
+}
+
+void APlayerShipBase::Serverrolling_Implementation(float timedelta)
+{
+	rolling(timedelta);
+}
+bool APlayerShipBase::Serverrolling_Validate(float timedelta)
+{
+	return true;
+}
+
+//events
+
+void APlayerShipBase::Collision(double dmg = 1)
+{
+	durability -= dmg;
+	UProgressBar* ProgressBar = dynamic_cast<UProgressBar*>(Widget->GetWidgetFromName(FName("ProgressBar_20")));
+	ProgressBar->SetPercent(durability / double(101));
+}
+
+void APlayerShipBase::Destruction()
+{
+	GetWorld()->SpawnActor<AActor>(BoomType, GetActorLocation(), GetActorRotation());
+	GetWorld()->DestroyActor(this);
+}
+
+void APlayerShipBase::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	if (OtherActor && (OtherActor != this) && OtherComp)
+	{
+		if (GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("CRASH"));
+		}
+		Collision();
+	}
+}
+
+//overrides
+void APlayerShipBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (durability < 0)
+	{
+		Destruction();
+	}
+}
+
+void APlayerShipBase::BeginPlay()
+{
+	Super::BeginPlay();
+	durability = 100;
+	Root->OnComponentHit.AddDynamic(this, &APlayerShipBase::OnHit);
+	Mesh2->OnComponentHit.AddDynamic(this, &APlayerShipBase::OnHit);
+	Mesh3->OnComponentHit.AddDynamic(this, &APlayerShipBase::OnHit);
+
+	Widget = CreateWidget<UUserWidget>(this->GetGameInstance(), WidgetClass);
+
+	InvWidget = CreateWidget<UUserWidget>(this->GetGameInstance(), InvWidgetClass);
+
+	Widget->AddToViewport();
+}
+
+
+
+
+// Called to bind functionality to input
+void APlayerShipBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	//register inputs
+	PlayerInputComponent->BindAction("Fire", IE_Pressed, this, &APlayerShipBase::ServerInitFire);
+	PlayerInputComponent->BindAction("Inv", IE_Pressed, this, &APlayerShipBase::ShowInv);
+	PlayerInputComponent->BindAction("Inv", IE_Released, this, &APlayerShipBase::HideInv);
+	PlayerInputComponent->BindAxis("thrust", this, &APlayerShipBase::Serverthrusting);
+	PlayerInputComponent->BindAxis("pitch", this, &APlayerShipBase::Serverpitching);
+	PlayerInputComponent->BindAxis("yaw", this, &APlayerShipBase::Serveryawing);
+	PlayerInputComponent->BindAxis("roll", this, &APlayerShipBase::Serverrolling);
+}
--- Source/ELOL/pawns/PlayerShipBase.h
+++ Source/ELOL/pawns/PlayerShipBase.h
@@ -4,14 +4,91 @@
 
 #include "CoreMinimal.h"
 #include "CameraPawn3D.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/ProgressBar.h"
 #include "PlayerShipBase.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
 class ELOL_API APlayerShipBase : public ACameraPawn3D
 {
 	GENERATED_BODY()
-	
+
+public:
+	// Sets default values for this pawn's properties
+	APlayerShipBase(); // ?E?
+
+	// Player Actions
+	void thrusting(float movementdelta);
+	void yawing(float movementdelta);
+	void pitching(float movementdelta);
+	void rolling(float movementdelta);
+	void InitFire();
+	void ShowInv();
+	void HideInv();
+
+	//server
+	UFUNCTION(server, unreliable, WithValidation)
+		void Serverthrusting(float movementdelta);
+	UFUNCTION(server, unreliable, WithValidation)
+		void ServerInitFire();
+	UFUNCTION(server, unreliable, WithValidation)
+		void Serverpitching(float movementdelta);
+	UFUNCTION(server, unreliable, WithValidation)
+		void Serveryawing(float movementdelta);
+	UFUNCTION(server, unreliable, WithValidation)
+		void Serverrolling(float movementdelta);
+
+	//events
+
+	void Collision(double dmg);
+
+	void Destruction();
+
+	UFUNCTION()
+		void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
+protected:
+
+	// Called when the game starts or when spawneds
+	virtual void BeginPlay() override;
+
+	//components
+	UPROPERTY(EditAnywhere)
+		UStaticMeshComponent* Mesh2;
+
+	UPROPERTY(EditAnywhere)
+		UStaticMeshComponent* Mesh3;
+
+	UPROPERTY(EditAnywhere)
+		UUserWidget* Widget;
+
+	UPROPERTY(EditAnywhere)
+		UUserWidget* InvWidget;
+
+	//properties
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		TSubclassOf<AActor> BulletType;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		TSubclassOf<AActor> BoomType;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		TSubclassOf<class UUserWidget> WidgetClass;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		TSubclassOf<class UUserWidget> InvWidgetClass;
+
+	//variables
+	UPROPERTY(EditAnywhere)
+		double durability;
+
+public:
+
+	//overrides
+	virtual void Tick(float DeltaTime) override;
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 };
