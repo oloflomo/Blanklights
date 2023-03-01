f280c932b54982049fb9adf54fc3fb184abb919a
oloflomo <strzalkowski.antoni@gmail.com>
2023-03-01 23:50:06 +0100

    player ship fix #2

---
+++ Source/ELOL/pawns/PlayerShipBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CameraPawn3D.h"
+#include "PlayerShipBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ELOL_API APlayerShipBase : public ACameraPawn3D
+{
+	GENERATED_BODY()
+	
+};
