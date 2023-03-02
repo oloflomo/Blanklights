f57f933102867908887f8e346d74aea4ccde43f5
oloflomo <strzalkowski.antoni@gmail.com>
2023-03-02 04:58:30 +0100

    pickup #1

---
+++ Source/ELOL/actors/pickup.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/StaticMeshComponent.h"
+#include "pickup.generated.h"
+
+UCLASS()
+class ELOL_API Apickup : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	Apickup();
+
+	//void Collision();
+
+	//UFUNCTION()
+	//	void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	//components
+	//UPROPERTY(EditAnywhere)
+	//	UStaticMeshComponent* Mesh;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
