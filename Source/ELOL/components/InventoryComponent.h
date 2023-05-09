8baf1a108904f8a1146dd8fd8f874dcec182b786
oloflomo <strzalkowski.antoni@gmail.com>
2023-05-09 03:26:11 +0200

    invnetory update 2

--- Source/ELOL/components/InventoryComponent.h
+++ Source/ELOL/components/InventoryComponent.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
 #include "ELOL/structs/InvItem.h"
+#include "ELOL/structs/InvItem.h"
 #include "InventoryComponent.generated.h"
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnInventoryUpdated);
